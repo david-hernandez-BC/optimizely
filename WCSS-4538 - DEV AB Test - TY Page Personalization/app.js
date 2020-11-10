ce76302d511d4f46f16f8d675b8552636ec2cd63
David Hernandez <david.hernandez@bigcommerce.com>
2020-11-10 14:23:38 -0600

    WCSS-4538 - DEV AB Test - TY Page Personalization

---
+++ WCSS-4538 - DEV AB Test - TY Page Personalization/app.js
@@ -0,0 +1,5 @@
+var modalLink = document.querySelector('.type-large > a:nth-of-type(1)');
+var button = document.querySelector('#ctaId');
+button.addEventListener('click', function () {
+    modalLink.click();
+});
